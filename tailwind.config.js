21737c6980ceaa2ce8be525967caab46c9a7eb00
zesanahmed <zesanahmed593@gmail.com>
2023-06-03 02:31:47 +0600

    components added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,33 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js'
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  themes: [
+    {
+      mytheme: {
+
+        "primary": "#570DF8",
+
+        "secondary": "#F000B8",
+
+        "accent": "#37CDBE",
+
+        "neutral": "#3D4451",
+
+        "base-100": "#FFFFFF",
+
+        "info": "#3ABFF8",
+
+        "success": "#36D399",
+
+        "warning": "#FBBD23",
+
+        "error": "#F87272",
+      },
+    },
+  ],
+  plugins: [require('daisyui')],
 }
 
