aeadc20516d4ff11ef1a626e00d58e3e9c6cf00e
zesanahmed <zesanahmed593@gmail.com>
2023-06-03 13:57:57 +0600

    job featured list added

--- src/components/JobList/JobList.jsx
+++ src/components/JobList/JobList.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import { SunIcon, CalculatorIcon } from '@heroicons/react/24/solid'
+import { SunIcon, CalculatorIcon, UserGroupIcon, WrenchIcon } from '@heroicons/react/24/solid'
 
 const JobList = () => {
     return (
         <div className='mt-16'>
             <h2 className='text-3xl font-bold '>Job Category List </h2>
             <p className='text-gray-500 mt-4'>Explore thousands of job opportunities with all the information you need. Its your future</p>
-            <div className='grid grid-cols-4 text-start mt-14'>
-                <div className='bg-violet-100'>
-                    <CalculatorIcon className="h-10 w-10 text-violet-400" />
-                    <h4>Account & Finance</h4>
-                    <p>300 Jobs Available</p>
+            <div className='grid grid-cols-4 text-start mt-14 gap-8'>
+                <div className='bg-violet-100 p-6 rounded-md'>
+                    <CalculatorIcon className="h-10 w-10 text-violet-400 mb-4" />
+                    <h4 className='font-bold'>Account & Finance</h4>
+                    <p className='text-gray-500 text-sm'>300 Jobs Available</p>
                 </div>
-                <div className=''>
-                    <SunIcon className="h-10 w-10 text-violet-400" />
-                    <h4>Creative Design</h4>
-                    <p>100+ Jobs Available</p>
+                <div className='bg-violet-100 p-6 rounded-md'>
+                    <SunIcon className="h-10 w-10 text-violet-400 mb-4" />
+                    <h4 className='font-bold'>Creative Design</h4>
+                    <p className='text-gray-500 text-sm'>100+ Jobs Available</p>
                 </div>
-                <div className=''>
-                    <SunIcon className="h-10 w-10 text-violet-400" />
-                    <h4>Marketing & Sales</h4>
-                    <p>150 Jobs Available</p>
+                <div className='bg-violet-100 p-6 rounded-md'>
+                    <UserGroupIcon className="h-10 w-10 text-violet-400 mb-4" />
+                    <h4 className='font-bold'>Marketing & Sales</h4>
+                    <p className='text-gray-500 text-sm'>150 Jobs Available</p>
                 </div>
-                <div >
-                    <SunIcon className="h-10 w-10 text-violet-400" />
-                    <h4>Engineering Job</h4>
-                    <p>224 Jobs Available</p>
+                <div className='bg-violet-100 p-6 rounded-md'>
+                    <WrenchIcon className="h-10 w-10 text-violet-400 mb-4" />
+                    <h4 className='font-bold'>Engineering Job</h4>
+                    <p className='text-gray-500 text-sm'>224 Jobs Available</p>
                 </div>
             </div>
         </div>
